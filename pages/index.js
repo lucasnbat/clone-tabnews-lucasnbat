4b203e8869a7645419f16409d4c14c288751d7ba
Lucas Nunes Batista <116975192+lucasnbat@users.noreply.github.com>
2024-07-17 02:15:58 +0000

    Adicionando pages/

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+function Home() {
+    return (
+        <div>Teste</div>
+    );
+}
+
+/* 
+ * export deafult: indica que ele é o primeiro a ser 
+ * renderizado e a partir dele todo o resto é renderizado 
+ */
+export default Home;
+
+/**
+ * Dá para ir na anteninha debaixo e 
+ * ir na porta (aba do terminal) e ir 
+ * na porta 3000 mudar a visibilidade 
+ * para public para voce acessar do
+ * celular se quiser
+ */
