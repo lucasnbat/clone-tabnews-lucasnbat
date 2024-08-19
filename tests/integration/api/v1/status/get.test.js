cc2138eacf57ffe3d388954bf728ccbf792bacc5
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-18 23:05:54 -0400

    adiciona primeiro teste de integração

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,7 @@
+/* get foi uma convensão do Filipe */
+
+test('GET to /api/v1/status should return 200', async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status")
+
+  expect(response.status).toBe(200)
+})
