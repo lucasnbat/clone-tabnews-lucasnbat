a52d063dea178a62e47aa1df564a4da12c12913b
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-09-23 09:48:09 -0400

    adjust on `database.js`

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   })
 
-  console.log('Credenciais POSTGRES:', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-
   try {
     await client.connect()
     const result = await client.query(queryObject)
